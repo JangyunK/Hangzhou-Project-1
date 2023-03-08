e23d61c2d8c983edab48528b08df8ad684ee267c
JangyunK <126736830+JangyunK@users.noreply.github.com>
2023-03-09 08:31:12 +0900

    Update Hangzhou project test1.py

--- Hangzhou project test1.py
+++ Hangzhou project test1.py
@@ -1,2 +1,16 @@
-print("Hello world!")
-print("for the first time in forever")
+import requests
+from bs4 import BeautifulSoup
+
+# Define the URL of Hangzhou City's Wikipedia page
+url = 'https://en.wikipedia.org/wiki/Hangzhou'
+
+# Make a request to the URL and get the HTML content
+response = requests.get(url)
+content = response.content
+
+# Use BeautifulSoup to parse the HTML content and extract the introduction section
+soup = BeautifulSoup(content, 'html.parser')
+intro_section = soup.find('div', {'class': 'mw-parser-output'}).p
+
+# Print out the introduction text
+print(intro_section.text.strip())
